ab649d174d9b5cea574a6185e0a3c5c3568c0ca2
farmei <farmei@proton.me>
2026-06-19 23:44:57 +0000

    add orangefox lunch target

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,11 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_rtwo.mk
+    $(LOCAL_DIR)/twrp_rtwo.mk \
+    $(LOCAL_DIR)/omni_rtwo.mk \
+    $(LOCAL_DIR)/orangefox_rtwo.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_rtwo-eng
+    twrp_rtwo-eng \
+    omni_rtwo-eng \
+    orangefox_rtwo-eng
---
+++ orangefox_rtwo.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/motorola/rtwo/twrp_rtwo.mk)
+
+PRODUCT_NAME := orangefox_rtwo
